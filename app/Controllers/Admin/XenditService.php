ecf6ef2a76ecae2ac5e8f9617c2c3a2acce5e874
MrcSatrio <marcsatrioff@gmail.com>
2023-07-31 17:48:58 +0700

    callback

---
+++ app/Controllers/Admin/XenditService.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Controllers\BaseController;
+
+
+class XenditService extends BaseController
+{
+    private $api_key = 'xnd_development_a5B7mjTf8h9JjInsKBlmLsHAHVufOy7K468Yw3aYrzDbotiSYsXmRQ091Boxb';
+
+    public function createInvoice($params)
+    {
+        $api_url = 'https://api.xendit.co/v2/invoices';
+
+        // Encode API Key ke Base64
+        $base64_encoded = base64_encode($this->api_key . ":");
+
+        // Sertakan dalam header Authorization
+        $auth_header = "Authorization: Basic " . $base64_encoded;
+
+        // Persiapkan data JSON untuk dikirim sebagai payload
+        $json_payload = json_encode($params);
+
+        // Konfigurasi cURL
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $api_url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', $auth_header));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json_payload);
+
+        // Eksekusi cURL dan dapatkan respons
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        // Parsing respons sebagai array
+        return json_decode($response, true);
+    }
+}
+
+$params = [
+    'external_id' => 'demo_147580196270',
+    'payer_email' => 'sample_email@xendit.co',
+    'description' => 'Trip to Bali',
+    'amount' => 32000,
+    'for-user-id' => '5c2323c67d6d305ac433ba20'
+];
+
+$xenditService = new \App\Controllers\Admin\XenditService();
+$xendit_response = $xenditService->createInvoice($params);
+
+var_dump($xendit_response);
--- app/Views/r_user/transaksi_formTopup.php
+++ app/Views/r_user/transaksi_formTopup.php
@@ -1,31 +1,44 @@
-    <?= $this->extend('template/index');
+<?= $this->extend('template/index'); ?>
 
-    $this->section('page_content'); ?>
+<?php $this->section('page_content'); ?>
 
-    <div class="card shadow mx-5">
-        <div class="card-body">
-            <h1 class="card-title">Top-Up</h1>
-            <?php if (!empty(session()->getFlashdata('error'))) : ?>
-                <div class="alert alert-danger" role="alert">
-                    <h4>Error</h4>
-                    </hr>
-                    <?php echo session()->getFlashdata('error'); ?>
+<div class="card shadow mx-5">
+    <div class="card-body">
+        <h1 class="card-title">Top-Up</h1>
+        <?php if (!empty(session()->getFlashdata('error'))) : ?>
+            <div class="alert alert-danger" role="alert">
+                <h4>Error</h4>
+                <hr>
+                <?= session()->getFlashdata('error'); ?>
+            </div>
+        <?php endif; ?>
+        <?php if (!empty(session()->getFlashdata('member'))) : ?>
+            <div class="alert alert-danger" role="alert">
+                <h4>Mohon Maaf, Tidak Bisa TopUp Saldo</h4>
+                <hr>
+                <?= session()->getFlashdata('member'); ?>
+            </div>
+        <?php endif; ?>
+        <form action="<?= base_url() ?>user/transaksi_topup" method="POST">
+            <div class="form-group row">
+                <label class="col-sm-3 col-form-label">Nomor Pokok Mahasiswa</label>
+                <div class="col-sm-9">
+                    <input type="text" readonly class="form-control" value="<?= $user['npm']; ?>" name="npm">
                 </div>
-            <?php endif; ?>
-            <?php if (!empty(session()->getFlashdata('member'))) : ?>
-                <div class="alert alert-danger" role="alert">
-                    <h4>Mohon Maaf, Tidak Bisa TopUp Saldo</h4>
-                    </hr>
-                    <?php echo session()->getFlashdata('member'); ?>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-3 col-form-label">Metode Pembayaran</label>
+                <div class="col-sm-9">
+                    <select name="jenis_pembayaran" class="form-control form-control-user custom-select" onchange="showCustomNominal(this.value)">
+                        <option selected>Pilih Metode Pembayaran</option>
+                        <option value="1">Cash</option>
+                        <option value="2">Ewallet (Instant)(Fee 2%)</option>
+                        <option value="3">Bank (Instant)(Fee 25%)</option>
+                        <option value="4">QRIS (Instant)(Fee 1%)</option>
+                    </select>
                 </div>
-            <?php endif; ?>
-            <form action="<?= base_url() ?>user/transaksi_topup" method="POST">
-                <div class="form-group row">
-                    <label class="col-sm-3 col-form-label">Nomor Pokok Mahasiswa</label>
-                    <div class="col-sm-9">
-                        <input type="text" readonly class="form-control" value="<?= $user['npm']; ?>" name="npm">
-                    </div>
-                </div>
+            </div>
+            <div id="nominalContainer"> <!-- Wrap the Nominal dropdown in a container -->
                 <div class="form-group row">
                     <label class="col-sm-3 col-form-label">Nominal</label>
                     <div class="col-sm-9">
@@ -39,33 +52,43 @@
                         </select>
                     </div>
                 </div>
+            </div>
+            <div id="customNominalContainer" style="display: none;">
                 <div class="form-group row">
-                    <label class="col-sm-3 col-form-label">Metode Pembayaran</label>
+                    <label class="col-sm-3 col-form-label">Nominal</label>
                     <div class="col-sm-9">
-                        <select name="jenis_pembayaran" class="form-control form-control-user custom-select">
-                            <option selected>Pilih Metode Pembayaran</option>
-                            <option value="1">Cash</option>
-                            <option value="2">Transfer</option>
-                        </select>
+                        <input type="number" name="nominal" class="form-control form-control-user" min="10000" placeholder="Masukan Nominal (Minimal 10.000)">
                     </div>
                 </div>
             </div>
-                    <!-- JENIS TRANSAKSI -->
-                    <div class="col-sm-9">
-                        <input type="hidden" class="form-control" value="1" name="jenis_transaksi">
-                    </div>
-                    <div class="col-sm-9">
-                        <input type="hidden" class="form-control" value="<?= $user['saldo']; ?>" name="saldoawal">
-                    </div>
-                    <div class="col-sm-9">
-                        <input type="hidden" class="form-control" value="1" name="status_transaksi">
-                    </div>
-                    <div class="col-sm-9 mt-4">
-                        <input type="submit" class="btn btn-primary">
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
 
-    <?= $this->endSection(); ?>
+            <div class="col-sm-9">
+                <input type="hidden" class="form-control" value="1" name="jenis_transaksi">
+                <input type="hidden" class="form-control" value="<?= $user['saldo']; ?>" name="saldoawal">
+                <input type="hidden" class="form-control" value="1" name="status_transaksi">
+            </div>
+            <div class="col-sm-9 mt-4">
+                <input type="submit" class="btn btn-primary">
+            </div>
+        </form>
+    </div>
+</div>
+<script>
+    function showCustomNominal(selectedValue) {
+        var customNominalContainer = document.getElementById('customNominalContainer');
+        var nominalContainer = document.getElementById('nominalContainer');
+        var nominalSelect = document.querySelector('select[name="nominal"]');
+
+        if (selectedValue === '1') { // Cash
+            customNominalContainer.style.display = 'none';
+            nominalContainer.style.display = 'block'; // Show the Nominal dropdown container
+            nominalSelect.disabled = false;
+        } else { // Ewallet, Bank, QRIS
+            customNominalContainer.style.display = 'block';
+            nominalContainer.style.display = 'none'; // Hide the Nominal dropdown container
+            nominalSelect.disabled = true;
+            nominalSelect.value = 'custom'; // Set the select to "Custom Nominal" option
+        }
+    }
+</script>
+<?= $this->endSection(); ?>
